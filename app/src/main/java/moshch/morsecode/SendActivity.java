21a6840e684e1cec4b011f77630b99aa80e9fe11
Nick Shch <ndnd@mail.ru>
2016-10-27 17:52:57 +0300

    Morse from keyboard and some refactor

--- app/src/main/java/moshch/morsecode/SendActivity.java
+++ app/src/main/java/moshch/morsecode/SendActivity.java
@@ -12,22 +12,95 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class SendActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager;
     private String mCameraId;
-    private Boolean isTorchOn;
+    private Boolean isFlashlightOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d("FlashLightActivity", "onCreate()");
+        Log.d("FlashlightActivity", "onCreate()");
         setContentView(R.layout.activity_send);
 
         Button buttonSendMessage = (Button) findViewById(R.id.button_sendMessage);
-        isTorchOn = false;
+        final EditText editText = (EditText) findViewById(R.id.sendingText);
+        isFlashlightOn = false;
 
+        if (!checkFlashlightIsAvailable()) {
+            return;
+        }
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isFlashlightOn) {
+                        turnOffFlashlight();
+                        isFlashlightOn = false;
+                    }
+                    //Morse rules:
+                    //The length of a dot is one unit.
+                    //A dash is three units.
+                    //The space between parts of the same letter is one unit.
+                    //The space between letters is three units.
+                    //The space between words is seven units.
+                    Thread t = new Thread() {
+                        public void run() {
+                            long unit = 300; //Delay in ms
+                            //String messageString = "...*---*...";
+                            String messageString = editText.getText().toString();
+                            try {
+                                for (int i = 0; i < messageString.length(); i++) {
+                                    switch (messageString.charAt(i)) {
+                                        case '.':  //dot
+                                            turnOnFlashlight();
+                                            sleep(unit);
+                                            turnOffFlashlight();
+                                            sleep(unit);
+                                            break;
+                                        case '-':  //dash
+                                            turnOnFlashlight();
+                                            sleep(3*unit);
+                                            turnOffFlashlight();
+                                            sleep(unit);
+                                            break;
+                                        case '*':  //space between letters
+                                            sleep(3*unit);
+                                            break;
+                                        case ' ':  //space between words
+                                            sleep(7*unit);
+                                            break;
+                                    }
+                                }
+                            } catch (Exception e){
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    t.start();
+                    if (isFlashlightOn) {
+                        turnOffFlashlight();
+                        isFlashlightOn = false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Boolean checkFlashlightIsAvailable() {
         Boolean isFlashAvailable = getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
@@ -45,70 +118,12 @@ public class SendActivity extends AppCompatActivity {
                 }
             });
             alert.show();
-            return;
+            return false;
+        } else {
+            return true;
         }
-        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try {
-            mCameraId = mCameraManager.getCameraIdList()[0];
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if (isTorchOn) {
-                        turnOffFlashLight();
-                        isTorchOn = false;
-                    }
-                    //Morse rules:
-                    //The length of a dot is one unit.
-                    //A dash is three units.
-                    //The space between parts of the same letter is one unit. The space between letters is three units.
-                    //The space between words is seven units.
-                    Thread t = new Thread() {
-                        public void run() {
-                            long unit = 300; //Delay in ms
-                            String myString = "...*---*...";
-                            try {
-                                for (int i = 0; i < myString.length(); i++) {
-                                    switch (myString.charAt(i)) {
-                                        case '.':  //dot
-                                            turnOnFlashLight();
-                                            sleep(unit);
-                                            turnOffFlashLight();
-                                            break;
-                                        case '-':  //dash
-                                            turnOnFlashLight();
-                                            sleep(3*unit);
-                                            turnOffFlashLight();
-                                            break;
-                                        case '*':  //space between letters
-                                            sleep(3*unit);
-                                            break;
-                                        case ' ':  //space between words
-                                            sleep(7*unit);
-                                            break;
-                                    }
-                                }
-                            } catch (Exception e){
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-                    t.start();
-                    if (isTorchOn) {
-                        turnOffFlashLight();
-                        isTorchOn = false;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
-    public void turnOnFlashLight() {
+    public void turnOnFlashlight() {
 
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -120,7 +135,7 @@ public class SendActivity extends AppCompatActivity {
     }
 
 
-    public void turnOffFlashLight() {
+    public void turnOffFlashlight() {
 
         try {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -135,24 +150,24 @@ public class SendActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        if(isTorchOn){
-            turnOffFlashLight();
+        if(isFlashlightOn){
+            turnOffFlashlight();
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(isTorchOn){
-            turnOffFlashLight();
+        if(isFlashlightOn){
+            turnOffFlashlight();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if(isTorchOn){
-            turnOnFlashLight();
+        if(isFlashlightOn){
+            turnOnFlashlight();
         }
     }
 }
